f39ec453416cc2c086415232dc58412e01a889ac
yingyugang <yingyugang@gmail.com>
2018-07-13 14:21:12 +0900

    amend Download framework.

---
+++ Assets/Framework_DL/Scripts/DownloadFramework/DownloadConstant.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+//TODO
+namespace BlueNoah.Download
+{
+    public static class DownloadConstant
+    {
+
+        public const string SERVER_ASSET_URL = "127.0.0.1/BlueNoah/Assets";
+
+        public static string APPLICATION_DATA_PATH
+        {
+            get
+            {
+#if UNITY_EDITOR
+                return Application.dataPath;
+#else
+                return Application.persistentDataPath;
+#endif
+            }
+        }
+
+        public static string DOWNLOAD_ASSET_PATH
+        {
+            get
+            {
+                return Path.Combine(APPLICATION_DATA_PATH, "DownloadAssets");
+            }
+        }
+    }
+}
